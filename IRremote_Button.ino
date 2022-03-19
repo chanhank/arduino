a51fc0c0f0b4d7df1074e444a5c6be261a620aa0
chanhank <100844657+chanhank@users.noreply.github.com>
2022-03-20 00:36:13 +0800

    Add files via upload

---
+++ IRremote_Button.ino
@@ -0,0 +1,116 @@
+
+#define DECODE_NEC          // Includes Apple and Onkyo
+#include <Arduino.h>
+#include "PinDefinitionsAndMore.h"
+#include <IRremote.hpp>
+#include <Arduino.h>
+#include "PinDefinitionsAndMore.h"
+#include <IRremote.hpp>
+const byte ledPin = 2;       // Builtin-LED pin
+const byte interruptPin = 0; // BOOT/IO0 button pin
+volatile byte state = LOW;
+uint16_t sAddress = 0x0102;
+uint8_t sCommand = 0x34;
+uint8_t sRepeats = 0;
+
+void setup() {
+    Serial.begin(115200);
+    // Just to know which program is running on my Arduino
+    Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
+
+    // Start the receiver and if not 3. parameter specified, take LED_BUILTIN pin from the internal boards definition as default feedback LED
+    IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+
+    Serial.print(F("Ready to receive IR signals of protocols: "));
+    printActiveIRProtocols(&Serial);
+    Serial.print(F("at pin "));
+    Serial.println(IR_RECEIVE_PIN);
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    Serial.begin(115200);
+
+    // Just to know which program is running on my Arduino
+    Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
+
+    /*
+     * The IR library setup. That's all!
+     */
+    IrSender.begin(); // Start with IR_SEND_PIN as send pin and if NO_LED_FEEDBACK_CODE is NOT defined, enable feedback LED at default feedback LED pin
+
+    Serial.print(F("Ready to send IR signals at pin "));
+    Serial.println(IR_SEND_PIN);
+    pinMode(ledPin, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
+    
+}
+
+
+void loop() {
+    /*
+     * Check if received data is available and if yes, try to decode it.
+     * Decoded result is in the IrReceiver.decodedIRData structure.
+     *
+     * E.g. command is in IrReceiver.decodedIRData.command
+     * address is in command is in IrReceiver.decodedIRData.address
+     * and up to 32 bit raw data in IrReceiver.decodedIRData.decodedRawData
+     */
+      /*
+     * Print current send values
+     */
+    if (IrReceiver.decode()) {
+
+        // Print a short summary of received data
+        IrReceiver.printIRResultShort(&Serial);
+        if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
+            // We have an unknown protocol here, print more info
+            IrReceiver.printIRResultRawFormatted(&Serial, true);
+        }
+        Serial.println();
+
+        /*
+         * !!!Important!!! Enable receiving of the next value,
+         * since receiving has stopped after the end of the current received data packet.
+         */
+        IrReceiver.resume(); // Enable receiving of the next value
+
+        /*
+         * Finally, check the received data and perform actions according to the received command
+         */
+        if (IrReceiver.decodedIRData.command == 0x10) {
+            // do something
+        } else if (IrReceiver.decodedIRData.command == 0x11) {
+            // do something else
+        }
+        Serial.println();
+    Serial.print(F("Send now: address=0x"));
+    Serial.print(sAddress, HEX);
+    Serial.print(F(" command=0x"));
+    Serial.print(sCommand, HEX);
+    Serial.print(F(" repeats="));
+    Serial.print(sRepeats);
+    Serial.println();
+
+    Serial.println(F("Send NEC with 16 bit address"));
+    Serial.flush();
+    IrSender.sendNEC(sAddress, sCommand, sRepeats);
+
+   Serial.println(F("Send NECRaw 0xCB340102"));
+   IrSender.sendNECRaw(0xCB340102, sRepeats);
+    
+    sAddress += 0x0101;
+    sCommand += 0x11;
+    sRepeats++;
+    // clip repeats at 4
+    if (sRepeats > 4) {
+        sRepeats = 4;
+    }
+
+    delay(1000);  // delay must be greater than 5 ms (RECORD_GAP_MICROS), otherwise the receiver sees it as one long signal
+    }
+    digitalWrite(ledPin, state);
+}
+
+void blink() {
+  state = !state;
+}
